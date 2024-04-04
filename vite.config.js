7fa6fbe713a2b6e40a969aa78f13511fb8110815
a.miyazaki <a.takahashi@flag-pictures.co.jp>
2024-04-04 10:05:32 +0900

    Set up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import pugPlugin from 'vite-plugin-pug'
+import tailwindcss from 'tailwindcss'
+import WindiCSS from "vite-plugin-windicss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    pugPlugin({
+      pretty: true,
+      handler: true
+    }),
+    WindiCSS(),
+    tailwindcss('./tailwind.config.js'),
+  ],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, './src/main.js'),
+      output: {
+        dir: resolve(__dirname, 'dist'),
+        entryFileNames: 'index.js',
+        chunkFileNames: '[name]-[hash].js',
+      },
+    },
+  },
+});
